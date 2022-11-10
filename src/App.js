51614fb1ede2d6c09e70d5d234b5e2fcb2c94771
mar-sh <marasi.af@gmail.com>
2022-11-10 11:55:54 +0700

    add base layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">Welcome</p>
+          <p className="sub-text">Connect your wallet to get started</p>
+          <button className="cta-button connect-wallet-button">
+            Connect Wallet
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
