cca82ebb3c91884ef2c94e9f2ce547cf790253b2
mar-sh <marasi.af@gmail.com>
2022-11-10 13:16:41 +0700

    display address and chain id when connected

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import AddressContainer from "./components/addressContainer";
 
 function App() {
   const [account, setAccount] = useState("");
+  const [chainId, setChainId] = useState("");
+
+  const getChainId = async () => {
+    try {
+      if (!window.ethereum) {
+        alert("Please install MetaMask browser extension to continue");
+      }
+
+      const { ethereum } = window;
+      const chainId = await ethereum.request({ method: "eth_chainId" });
+
+      setChainId(chainId);
+    } catch (error) {
+      console.error(error);
+      alert("Something went wrong. Please try again later.");
+    }
+  };
 
   const connectWallet = async () => {
     try {
@@ -17,6 +34,8 @@ function App() {
       const [account] = await ethereum.request({
         method: "eth_requestAccounts",
       });
+
+      await getChainId();
 
       setAccount(account);
     } catch (error) {
@@ -38,6 +57,8 @@ function App() {
           method: "eth_accounts",
         });
 
+        await getChainId();
+
         setAccount(account);
       } catch (error) {
         console.error(error);
@@ -48,19 +69,35 @@ function App() {
     checkConnectedWallet();
   }, []);
 
+  const renderContent = () => {
+    if (account) {
+      return (
+        <div>
+          <p className="header gradient-text">Connected</p>
+          <p className="sub-text">Chain ID: {chainId}</p>
+          <AddressContainer address={account}></AddressContainer>
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <p className="header gradient-text">Welcome</p>
+        <p className="sub-text">Connect your wallet to get started</p>
+        <button
+          onClick={connectWallet}
+          className="cta-button connect-wallet-button"
+        >
+          Connect Wallet
+        </button>
+      </>
+    );
+  };
+
   return (
     <div className="App">
       <div className="container">
-        <div className="header-container">
-          <p className="header gradient-text">Welcome</p>
-          <p className="sub-text">Connect your wallet to get started</p>
-          <button
-            onClick={connectWallet}
-            className="cta-button connect-wallet-button"
-          >
-            Connect Wallet
-          </button>
-        </div>
+        <div className="header-container">{renderContent()}</div>
       </div>
     </div>
   );
--- src/components/addressContainer.js
+++ src/components/addressContainer.js
@@ -4,7 +4,7 @@ import styles from "./addressContainer.module.css";
 const AddressContainer = ({ address }) => {
   return (
     <div className={styles.address_container}>
-      <p className={styles.address_container_text}>{address}</p>
+      <p className={styles.address_container_text}>Address : {address}</p>
       <button
         className={styles.address_container_button}
         onClick={() => navigator.clipboard.writeText(address)}
